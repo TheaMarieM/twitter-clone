cab5e2e1ffdd0e4c7db3cf2a8b831518299e6b81
TheaMarieM <magnomarithea157@gmail.com>
2025-11-25 03:35:04 +0800

    Update project files and documentation

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'url' => env('MYSQL_URL'), // <-- THIS IS THE KEY CHANGE
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
